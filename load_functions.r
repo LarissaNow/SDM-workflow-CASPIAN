0f2a18b73df522cd25d1bac0aa325363e93eaa07
LarissaNow <74062495+LarissaNow@users.noreply.github.com>
2021-08-19 16:06:23 +0200

    Add files via upload
    updated on 19-08-21; loads more functions

--- load_functions.r
+++ load_functions.r
@@ -13,13 +13,19 @@
 source("userdatacheck.r")
 source("GBIFdownclean.r")
 source("addenvir2.r")
+source("addenvirLC2.r")
 source("PAsampleParallel.r")
+source("PAsampleParallelLC.r")
 
 # model fitting and validation
 source("GAMfitting.r")
+source("GAMfittingLC2.r")
 
 # suitability prediction
 source("loadBaseEnv.r")
+source("loadBaseEnvLC2.r")
 source("predictGAMParallel.r")
+source("predictGAMParallelLC.r")
 source("modelaverageParallel.r")
+source("modelaverageParallelLC.r")
 source("plotSuitabilities.r")
